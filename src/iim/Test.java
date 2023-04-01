588202d360a7aa531dd368d471b4f51e7eab276c
altaifeizi <altaifeizi@macbook-air>
2023-04-01 15:46:19 +0200

    guter Ordnershit

--- src/iim/Test.java
+++ src/iim/Test.java
@@ -2,11 +2,14 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
+package iim;
 
 /**
  *
- * @author frey
+ * @author altaifeizi
  */
 public class Test {
-    
+    public static void hallo() {
+        System.out.print("Jü");
+    }
 }

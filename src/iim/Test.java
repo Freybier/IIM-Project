f21b39228b9ff6d0daf178d127b2b1e3d75802f3
Yann Leymann <Yann Leymann@ROYALBEEF.speedport.ip>
2023-07-17 10:55:35 +0200

    Kickoff try

--- src/iim/Test.java
+++ src/iim/Test.java
@@ -1,4 +1,4 @@
-package iim.projekt;
+
 
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
@@ -11,12 +11,9 @@ package iim;
  * @author altaifeizi
  */
 public class Test {
-<<<<<<<< HEAD:src/iim/Test.java
-    public static void hallo() {
-        System.out.print("Jü");
-========
-    public static void testmethode(){
+
+    public static void hallo(){
         System.out.println("Das is ein Test");
->>>>>>>> main:src/iim/projekt/Test.java
+
     }
 }

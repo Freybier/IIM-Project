e5ddf2ac5cf7ae73c66273d495d9fb677efa4860
Yann Leymann <Yann Leymann@ROYALBEEF.speedport.ip>
2023-08-09 11:41:48 +0200

    unnötiges entfernt

--- src/iim/pvZeiten/ProfToCSV.java
+++ src/iim/pvZeiten/ProfToCSV.java
@@ -18,7 +18,6 @@ public class ProfToCSV {
             writer.append("Name,Monatg 1. Block,Monatg 2. Block,Monatg 3. Block,Monatg 4. Block,Monatg 5. Block,Monatg 6. Block,Dienstag 1. Block,Dienstag 2. Block,Dienstag 3. Block,Dienstag 4. Block,Dienstag 5. Block,Dienstag 6. Block,Mittwoch 1. Block,Mittwoch 2. Block,Mittwoch 3. Block,Mittwoch 4. Block,Mittwoch 5. Block,Mittwoch 6. Block,Donnerstag 1. Block,Donnerstag 2. Block,Donnerstag 3. Block,Donnerstag 4. Block,Donnerstag 5. Block,Donnerstag 6. Block,Freitag 1. Block,Freitag 2. Block,Freitag 3. Block,Freitag 4. Block,Freitag 5. Block,Freitag 6. Block,Samstag 1. Block,Samstag 2. Block,Samstag 3. Block,Samstag 4. Block\n");
 
             String[] tage = {"Montag", "Dienstag", "Mittwoch", "Donnerstag", "Freitag", "Samstag"};
-            String[] blocks = {"1. block", "2. block", "3. block", "4. block", "5. block", "6. block"};
             
             for (Professor professor : professoren) {
             StringBuilder line = new StringBuilder(professor.getName());
@@ -28,14 +27,19 @@ public class ProfToCSV {
                 char zeit = professor.getZeiten().charAt(i);
                 String status;
 
-                if (zeit == ' ') {
-                    status = "verfügbar";
-                } else if (zeit == 'X') {
-                    status = "kann nicht";
-                } else if (zeit == 'x') {
-                    status = "will nicht";
-                } else {
-                    status = "";
+                switch (zeit) {
+                    case ' ':
+                        status = "verfügbar";
+                        break;
+                    case 'X':
+                        status = "kann nicht";
+                        break;
+                    case 'x':
+                        status = "will nicht";
+                        break;
+                    default:
+                        status = "";
+                        break;
                 }
                 
                 if (i % 6 == 0) {
@@ -63,7 +67,6 @@ public class ProfToCSV {
             
             writer.flush();
         } catch (IOException e) {
-            e.printStackTrace();
         }
     }
 }

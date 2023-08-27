235f1ad52c1a0cde137b71dcb053738588f3fa8a
Yann Leymann <Yann Leymann@ROYALBEEF.speedport.ip>
2023-08-27 12:38:38 +0200

    ein verzweifelter versuche ein bisschen Logik einzubauen

--- src/GUI/TableTransferHandler.java
+++ src/GUI/TableTransferHandler.java
@@ -4,15 +4,31 @@
  */
 package GUI;
 
+import iim.Hochschule.Dozent;
+import iim.Hochschule.LV;
+import iim.Hochschule.Zug;
 import javax.swing.*;
 import java.awt.datatransfer.*;
 import java.io.IOException;
+import java.util.List;
 
 public class TableTransferHandler extends TransferHandler {
-    private JTable jTable;
 
-    public TableTransferHandler(JTable jTable) {
+    private JTable jTable;
+    private List<LV> lvList;
+    private String dozentenName;
+    private List<Dozent> dozentenList;
+    private List<Zug> zugList;
+    
+    public TableTransferHandler(JTable jTable, List<LV> lvList, List<Dozent> dozentenList, List<Zug> zugList) {
         this.jTable = jTable;
+        this.lvList = lvList;
+        this.dozentenList = dozentenList;
+        this.zugList = zugList;
+    }
+    
+    public void setDozentenName(String dozentenName){
+        this.dozentenName = dozentenName;
     }
 
     @Override
@@ -34,6 +50,21 @@ public class TableTransferHandler extends TransferHandler {
         Transferable transferable = support.getTransferable();
         try {
             String data = (String) transferable.getTransferData(DataFlavor.stringFlavor);
+            //DataFlavor customDataFlavor = new DataFlavor(LV.class, "Custom LV Object");
+            //LV lvObjekt = (LV) transferable.getTransferData(customDataFlavor);
+            System.out.println(data);
+            for (LV lv : lvList) {
+                if (lv.getName().equals(data) && lv.getDozentName().equals(dozentenName)) {
+                    long scheduled = 1;
+                    int check = 33 - ((col * 6) - 6) - row;
+                    scheduled = scheduled << check;
+                    scheduled = scheduled & lv.getScheduledLV();
+                    lv.setScheduledLV(scheduled);
+                    System.out.println("LVScheduled wurde geseted");
+
+                }
+            }
+
             jTable.setValueAt(data, row, col);
             return true;
         } catch (UnsupportedFlavorException | IOException e) {

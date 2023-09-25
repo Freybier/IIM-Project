ce418922cbdebc20914c023fe8d73a0eefb352e3
Freybier <lukas.frey@quant-x-sec.com>
2023-09-25 14:47:30 +0200

    added comments in main

--- src/iim/projekt/IIMProjekt.java
+++ src/iim/projekt/IIMProjekt.java
@@ -30,15 +30,21 @@ public class IIMProjekt {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        // gets path till "IIM-Projekt", needed to be started via jar-file
         String absolutPath = System.getProperty("user.dir");
-        String relativePath = absolutPath + "\\src\\iim\\pvZeiten\\pvZeiten.txt";
-
+        String pvzeiten = absolutPath + "\\src\\iim\\pvZeiten\\pvZeiten.txt";
+        
         String handtuchCSVFilePath = absolutPath + "\\src\\iim\\Handtuch\\HandtuchNeu.csv";
-        String fileReadPath = absolutPath + "\\src\\iim\\Handtuch\\Handtuch.html";
-        ReadHandtuch2.readFromFile(fileReadPath);
+        String handtuchHTMLReadPath = absolutPath + "\\src\\iim\\Handtuch\\Handtuch.html";
+        
+        // differentialte if you need to read from file or if you need to read from a browser
+        ReadHandtuch2.readFromFile(handtuchHTMLReadPath);
         //ReadHandtuch2.readFromWebsite("file:///C:/Users/Frey/Downloads/Handtuch%20(1).html");
-
-        List<Dozent> dozentList = pvZeitenToDozent.splittNameWishList(relativePath);
+        
+        // creates Dozenten-list from file pvZeiten
+        List<Dozent> dozentList = pvZeitenToDozent.splittNameWishList(pvzeiten);
+        
+        // creates foundation for creating the other Lists from Handtuch 
         ReadCSVs.createObjects(handtuchCSVFilePath, dozentList);
         
         List<LV> lvList = ReadCSVs.getLVList();
